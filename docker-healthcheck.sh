92db5c30dfd20274b8a2d34921bcc214b0abad48
Jacob Alberty <jacob.alberty@vetmanage.com>
2017-10-18 11:04:53 -0500

    Check for existence of system.properties before trying to read it in healthcheck

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 SYSPROPS_FILE=${DATADIR}/system.properties
-SYSPROPS_PORT=`grep "^unifi.https.port=" ${SYSPROPS_FILE} | cut -d'=' -f2`
+if [ -f "${SYSPROPS_FILE}" ]; then
+    SYSPROPS_PORT=`grep "^unifi.https.port=" ${SYSPROPS_FILE} | cut -d'=' -f2`
+fi
 PORT=${SYSPROPS_PORT:-8443}
 
 curl -k -L --fail https://localhost:${PORT}

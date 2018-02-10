601333e071abc8c4f7b0865dbc80a7e8dfff1ee2
Jacob Alberty <jacob.alberty@vetmanage.com>
2018-02-10 11:31:14 -0600

    Use latest mongodb because 2.6 was EOL'd long ago
    Fixes #101

--- docker-build.sh
+++ docker-build.sh
@@ -7,14 +7,18 @@ fi
 
 apt-get update
 apt-get install -qy --no-install-recommends \
+    apt-transport-https \
     curl \
     openjdk-8-jre-headless \
     procps \
     libcap2-bin
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+apt-get update
 echo "deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti" > /etc/apt/sources.list.d/20ubiquiti.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
 curl -L -o ./unifi.deb "${1}"
-apt -qy install ./unifi.deb
+apt -qy install mongodb-org ./unifi.deb
 rm -f ./unifi.deb
 chown -R unifi:unifi /usr/lib/unifi
 rm -rf /var/lib/apt/lists/*

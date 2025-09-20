465e5eda39c6cb8d0ed42c685501016b0df342e9
lionelwidmer <36642667+lionelwidmer@users.noreply.github.com>
2025-09-20 21:57:47 +0200

    Add variable SMTP_STARTTLS_ENABLED

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -150,6 +150,10 @@ if ! [[ -z "$UNIFI_HTTPS_PORT"  ]]; then
   settings["unifi.https.port"]="$UNIFI_HTTPS_PORT"
 fi
 
+if ! [[ -z "$SMTP_SLARTTLS_ENABLED"  ]]; then
+  settings["smtp.starttls_enabled"]="SMTP_SLARTTLS_ENABLED"
+fi
+
 if [[ "$UNIFI_ECC_CERT" == "true" ]]; then
   settings["unifi.https.sslEnabledProtocols"]="TLSv1.2"
   settings["unifi.https.ciphers"]="TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
